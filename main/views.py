79a20463eb9eb5804332c4e9b845fc19f2e9e774
Alena <alena.nikolskaya.1994@mail.ru>
2020-04-19 19:37:54 +0300

    Результат второго урока

--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+
+data = {
+    'lists': [
+        {'name': 'Работа', 'is_done': True},
+        {'name': 'Дом', 'is_done': True},
+        {'name': 'Учеба', 'is_done':False}
+    ],
+    'user_name': 'Admin',
+}
+
+
 def main_view(request):
-    return render(request, 'index.html')
+    context = data
+    return render(request, 'index.html', context)
